15636c275af622b9ab5d631d825312aecaf460b0
selwy6208 <hamilton6208@gmail.com>
2023-09-21 07:56:53 +0900

    add payment button

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,11 +1,12 @@
 import React, { FC }  from 'react';
-import { CardField } from '@stripe/stripe-react-native';
+import { CardField, useStripe } from '@stripe/stripe-react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const PaymentScreen: FC = () => {
-	// const { confirmPayment } = useStripe();
+	const { confirmPayment } = useStripe();
 
 	return (
-		<CardField
+		<><CardField
 			postalCodeEnabled={true}
 			placeholders={{
 				number: '6208 6208 6208 6208',
@@ -18,17 +19,40 @@ const PaymentScreen: FC = () => {
 			// eslint-disable-next-line react-native/no-inline-styles
 			style={{
 				width: '100%',
-        height: 50,
-        marginVertical: 30,
+				height: 50,
+				marginVertical: 30,
 			}}
 			onCardChange={(cardDetails) => {
 				console.log('cardDetails', cardDetails);
-			}}
+			} }
 			onFocus={(focusedField) => {
 				console.log('focusField', focusedField);
-			}}
-		 />
+			} } />
+			<TouchableOpacity
+				onPress={() => confirmPayment}
+				style={styles.buttonStyle}>
+				<Text style={styles.buttonText}>
+					Pay
+				</Text>
+			</TouchableOpacity></>
 	);
 };
+
+const styles = StyleSheet.create({
+	buttonStyle: {
+		alignItems: 'center',
+		backgroundColor: '#b44666',
+		padding: 14,
+		marginLeft: 6,
+		marginRight: 6,
+		marginBottom: 20,
+		borderRadius: 3,
+	},
+	buttonText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: '#fff',
+	},
+});
 
 export default PaymentScreen;
